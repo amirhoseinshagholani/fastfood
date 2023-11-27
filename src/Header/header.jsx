555524f670bd4d83273cc9ce330beada916acf11
AmirHosein <sh.amirhosein@gmail.com>
2023-11-28 00:27:37 +0330

    create header

---
+++ src/Header/header.jsx
@@ -0,0 +1,16 @@
+import './header.css';
+
+const Header = ({a}) => {
+    return (
+        <header className='bg-size-cover bg-position-center py-3'>
+            <div className='container'>
+                <h1 className='text-start text-light'>
+                    FAST FOOD CATALOGUE
+                </h1>
+            </div>
+        </header>
+        
+    )
+}
+
+export default Header;
