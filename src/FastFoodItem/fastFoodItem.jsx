74909952324fb274a15721c46bbc3ec5902b59b6
AmirHosein <sh.amirhosein@gmail.com>
2023-12-09 23:35:45 +0330

    card_placeholder

--- src/FastFoodItem/fastFoodItem.jsx
+++ src/FastFoodItem/fastFoodItem.jsx
@@ -1,3 +1,5 @@
+import './fastFoodItem.css';
+import {HiShoppingCart} from 'react-icons/hi';
 
 const FastFoodItem = ({name,price,ingredients,imageUrl}) =>{
     return(
@@ -5,12 +7,15 @@ const FastFoodItem = ({name,price,ingredients,imageUrl}) =>{
             <span className="badge badge-end badge-shadow bg-success fs-md fw-medium">
                 قیمت: {price.toLocaleString()} تومان
             </span>
-            <img className="card-img-top" src={imageUrl}/>
+            <div className='card__placeholder'>
+                <img className="card-img-top" src={imageUrl}/>
+            </div>
             <div className="card-body text-center pt-3 pb-4 d-flex flex-column">
                 <h5 className="mb-2">{name}</h5>
                 <div className="fs-sm fw-bold text-muted mb-3">{ingredients}</div>
             </div>
             <button className="btn btn-outline-success btn-sm w-100 mt-auto fw-bold">
+                <HiShoppingCart className='fs-5 ms-3'></HiShoppingCart>
                 افزودن به سبد خرید
             </button>
         </div>
